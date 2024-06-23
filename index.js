e64c11c0631bc7389d875b615b9735dab5c54adb
varbuzov <varbuzov9@gmail.com>
2024-06-22 20:34:41 -0400

    2024/06/22 20:25

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    
+    const currentTime = new Date().toISOString();
+    res.write('<html><body>');
+    res.write('<h1>Vladimir Arbuzov - </h1>');
+    res.write(`${currentTime}`);
+    res.write('</body></html>');
+    
+    res.end();
+});
+
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
