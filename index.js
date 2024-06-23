8f1b428e3411a339d99c88ec108f0fc1cd1ada80
varbuzov <varbuzov9@gmail.com>
2024-06-22 20:59:29 -0400

    22/06/2024 21:00

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
     
     const currentTime = new Date().toISOString();
     res.write('<html><body>');
